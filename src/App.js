3e72d4cb2a352a8348ea9145f26a9e15f7884260
Agastya Makkar <agastya.makkar@gmail.com>
2021-08-03 03:28:46 +0530

    Partially completed one component.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
+import React from "react";
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export default class Game extends React.Component {
+  constructor(props) {
+    super(props);
+  }
 
-export default App;
+  render() {
+    return (
+      <div className="pane">
+        <h1>Here when you want to get away.</h1>
+        <p>Which one are you today?</p>
+        <div className="clearfix">
+          <div className="box">
+            <button className="button" onClick={() => { console.log("Pressed") }}>
+              <em>
+                “I don’t know where but I <b>do</b> know where. I’ve been there in my dreams but I don’t know its <b>name</b>.”
+              </em>
+            </button>
+          </div>
+          <div className="box">
+            <button className="button" onClick={() => { console.log("Pressed") }}>
+              <em>
+                “I know <b>exactly</b> where I want to go and I can’t wait.”
+              </em>
+            </button>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
